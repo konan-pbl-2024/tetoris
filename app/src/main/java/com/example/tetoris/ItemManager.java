5d33ab2a87eb8a82b7cb9a9b77b8a23c35d6b106
shido55 <shidodo2004@gmail.com>
2024-11-15 13:57:18 +0900

    ・サンプルになる動画を追加 ・お邪魔ミノの出現間隔をランダムに設定 ・ステージ選択画面のレイアウトを変更

--- app/src/main/java/com/example/tetoris/ItemManager.java
+++ app/src/main/java/com/example/tetoris/ItemManager.java
@@ -18,8 +18,8 @@ public class ItemManager {
         itemList.add(new Items("Score Booster", 12, 8,false,false));//1.2,1.2
         itemList.add(new Items("Score Booster2", 20, 5,false,false));//2.0,1.5
         itemList.add(new Items("Speed Booster", 9, 12,false,false));//0.8,0.8
-        itemList.add(new Items("Speed Booster2", 8, 15,true,false));//0.6,0.5
-        itemList.add(new Items("holddis",15,10,true,false));//1,1
+        itemList.add(new Items("Speed Booster2", 8, 15,false,false));//0.6,0.5
+        itemList.add(new Items("holddis",20,10,true,false));//1,1
         itemList.add(new Items("obstacle",15,10,false,true));//1,1
         itemList.add(new Items("obstacle2",25,8,false,true));//1,1
         itemList.add(new Items("boss",30,8,true,true));//1,1
--- app/src/main/java/com/example/tetoris/OpActivity.java
+++ app/src/main/java/com/example/tetoris/OpActivity.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ScrollView;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,7 +25,7 @@ public class OpActivity extends AppCompatActivity {
 
 
         //
-        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.sample3);
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.samplemovie);
 
         // VideoViewに動画をセット
         videoView.setVideoURI(videoUri);
